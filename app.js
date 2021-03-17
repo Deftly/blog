4731e21318c8b0ffc8e1b222bc434ccf0931362f
Deftly <deftkz@gmail.com>
2021-03-16 18:30:54 -0700

    added gitignore, Procfile, and update ports

--- app.js
+++ app.js
@@ -61,6 +61,11 @@ app.get("*", (req, res) => {
   res.send("404 Page not found")
 })
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server successfully started");
 });
